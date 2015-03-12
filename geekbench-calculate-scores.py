3e9e41379772c713e261af85f9136e521c563bbc
Tharaga Balachandran <tharaga@primatelabs.com>
2015-03-12 17:37:11 -0400

    Replace all tabs with spaces

--- geekbench-calculate-scores.py
+++ geekbench-calculate-scores.py
@@ -44,174 +44,174 @@ baseline_rate = {"AES" : 2297304827.712579, "Twofish" : 147104771.2752039,
 "Stream Triad" : 11796840626.258623 }
 
 class Workload(object):
-	def __init__(self, jsonf):
-		self.name = jsonf['name']
-		if len(jsonf['results']) == 2:
-			self.sc_runtime = jsonf['results'][0]['runtime']
-			self.mc_runtime = jsonf['results'][1]['runtime']
-			self.sc_work = jsonf['results'][0]['work']
-			self.mc_work = jsonf['results'][1]['work']
-		elif jsonf['results'][0]['threads'] == 1:
-			self.sc_runtime = jsonf['results'][0]['runtime']
-			self.mc_runtime = 0
-			self.sc_work = jsonf['results'][0]['work']
-			self.mc_work = 0
-		elif jsonf['results'][0]['threads'] > 1:
-			self.sc_runtime = 0
-			self.mc_runtime = jsonf['results'][0]['runtime']
-			self.sc_work = 0
-			self.mc_work = jsonf['results'][0]['work']
+  def __init__(self, jsonf):
+    self.name = jsonf['name']
+    if len(jsonf['results']) == 2:
+      self.sc_runtime = jsonf['results'][0]['runtime']
+      self.mc_runtime = jsonf['results'][1]['runtime']
+      self.sc_work = jsonf['results'][0]['work']
+      self.mc_work = jsonf['results'][1]['work']
+    elif jsonf['results'][0]['threads'] == 1:
+      self.sc_runtime = jsonf['results'][0]['runtime']
+      self.mc_runtime = 0
+      self.sc_work = jsonf['results'][0]['work']
+      self.mc_work = 0
+    elif jsonf['results'][0]['threads'] > 1:
+      self.sc_runtime = 0
+      self.mc_runtime = jsonf['results'][0]['runtime']
+      self.sc_work = 0
+      self.mc_work = jsonf['results'][0]['work']
 
-	def sc_rate(self):
-		if self.sc_runtime > 0:
-			return self.sc_work / self.sc_runtime
-		return 0
+  def sc_rate(self):
+    if self.sc_runtime > 0:
+      return self.sc_work / self.sc_runtime
+    return 0
 
-	def mc_rate(self):
-		if self.mc_runtime > 0:
-			return self.mc_work / self.mc_runtime
-		return 0
+  def mc_rate(self):
+    if self.mc_runtime > 0:
+      return self.mc_work / self.mc_runtime
+    return 0
 
-	def wl_sc_score(self):
-		rel = self.sc_rate() / baseline_rate[self.name]
-		return int(2500 * rel)
+  def wl_sc_score(self):
+    rel = self.sc_rate() / baseline_rate[self.name]
+    return int(2500 * rel)
 
-	def wl_mc_score(self):
-		rel = self.mc_rate() / baseline_rate[self.name]
-		return int(2500 * rel)
+  def wl_mc_score(self):
+    rel = self.mc_rate() / baseline_rate[self.name]
+    return int(2500 * rel)
 
 def get_sections(jsondata):
-	sections = {}
-	ss = jsondata['sections']
-	for s in ss:
-		wls = s['workloads']
-		section_name = s['name']
-		section = []
-		for wl in wls:
-			section.append(Workload(wl))
-		sections[section_name] = section
-	return sections
+  sections = {}
+  ss = jsondata['sections']
+  for s in ss:
+    wls = s['workloads']
+    section_name = s['name']
+    section = []
+    for wl in wls:
+      section.append(Workload(wl))
+    sections[section_name] = section
+  return sections
 
 def compute_sect_sc_score(section):
-	scores = [wl.wl_sc_score() for wl in section]
-	return int(scipy.stats.gmean(scores))
+  scores = [wl.wl_sc_score() for wl in section]
+  return int(scipy.stats.gmean(scores))
 
 def compute_sect_mc_score(section):
-	scores = [wl.wl_mc_score() for wl in section]
-	return int(scipy.stats.gmean(scores))
+  scores = [wl.wl_mc_score() for wl in section]
+  return int(scipy.stats.gmean(scores))
 
 def compute_geekbench_sc_score(sections):
-	geekbench_score = 0
-	try:
-		geekbench_score = (0.4 * compute_sect_sc_score(sections['Integer'])) + \
-		(0.4 * compute_sect_sc_score(sections['Floating Point'])) + \
-		(0.2 * compute_sect_sc_score(sections['Memory']))
-	except KeyError:
-		pass
-	return int(geekbench_score)
+  geekbench_score = 0
+  try:
+    geekbench_score = (0.4 * compute_sect_sc_score(sections['Integer'])) + \
+    (0.4 * compute_sect_sc_score(sections['Floating Point'])) + \
+    (0.2 * compute_sect_sc_score(sections['Memory']))
+  except KeyError:
+    pass
+  return int(geekbench_score)
 
 def compute_geekbench_mc_score(sections):
-	geekbench_score = 0
-	try:
-		geekbench_score = (0.4 * compute_sect_mc_score(sections['Integer'])) + \
-		(0.4 * compute_sect_mc_score(sections['Floating Point']))  + \
-		(0.2 * compute_sect_mc_score(sections['Memory']))
-	except KeyError:
-		pass
-	return int(geekbench_score)
+  geekbench_score = 0
+  try:
+    geekbench_score = (0.4 * compute_sect_mc_score(sections['Integer'])) + \
+    (0.4 * compute_sect_mc_score(sections['Floating Point']))  + \
+    (0.2 * compute_sect_mc_score(sections['Memory']))
+  except KeyError:
+    pass
+  return int(geekbench_score)
 
 def get_rate_string(wl_name, rate):
 
-	rate_units = { "BytesSecond" : "B/sec", "Flops" : "flops",
-	"AllocSecond" : "allocs/second", "PixelsSecond" : "pixels/sec",
-	"NodesSecond" : "nodes/sec", "PairsSecond" : "pairs/sec"}
+  rate_units = { "BytesSecond" : "B/sec", "Flops" : "flops",
+  "AllocSecond" : "allocs/second", "PixelsSecond" : "pixels/sec",
+  "NodesSecond" : "nodes/sec", "PairsSecond" : "pairs/sec"}
 
-	wl_units = {"AES" : rate_units['BytesSecond'],
-	"Twofish" : rate_units['BytesSecond'], "SHA1" : rate_units['BytesSecond'],
-	"SHA2" : rate_units['BytesSecond'],
-	"BZip2 Compress" : rate_units['BytesSecond'],
-	"BZip2 Decompress" : rate_units['BytesSecond'],
-	"JPEG Compress" :  rate_units['PixelsSecond'],
-	"JPEG Decompress" : rate_units['PixelsSecond'],
-	"PNG Compress" : rate_units['PixelsSecond'],
-	"PNG Decompress" : rate_units['PixelsSecond'],
-	"Sobel" : rate_units['PixelsSecond'],
-	"Lua" : rate_units['BytesSecond'] ,
-	"Dijkstra" : rate_units['PairsSecond'],
-	"BlackScholes" : rate_units['NodesSecond'],
-	"Mandelbrot" : rate_units['Flops'],
-	"Sharpen Filter" :   rate_units['Flops'],
-	"Blur Filter" :  rate_units['Flops'],
-	"SGEMM" : rate_units['Flops'], "DGEMM" :  rate_units['Flops'] ,
-	"SFFT" : rate_units['Flops'], "DFFT" :  rate_units['Flops'],
-	"N-Body" : rate_units['PairsSecond'],
-	"Ray Trace" : rate_units['PixelsSecond'],
-	"Stream Copy" : rate_units['BytesSecond'],
-	"Stream Scale" : rate_units['BytesSecond'],
-	"Stream Add" :  rate_units['BytesSecond'],
-	"Stream Triad" : rate_units['BytesSecond'] }
+  wl_units = {"AES" : rate_units['BytesSecond'],
+  "Twofish" : rate_units['BytesSecond'], "SHA1" : rate_units['BytesSecond'],
+  "SHA2" : rate_units['BytesSecond'],
+  "BZip2 Compress" : rate_units['BytesSecond'],
+  "BZip2 Decompress" : rate_units['BytesSecond'],
+  "JPEG Compress" :  rate_units['PixelsSecond'],
+  "JPEG Decompress" : rate_units['PixelsSecond'],
+  "PNG Compress" : rate_units['PixelsSecond'],
+  "PNG Decompress" : rate_units['PixelsSecond'],
+  "Sobel" : rate_units['PixelsSecond'],
+  "Lua" : rate_units['BytesSecond'] ,
+  "Dijkstra" : rate_units['PairsSecond'],
+  "BlackScholes" : rate_units['NodesSecond'],
+  "Mandelbrot" : rate_units['Flops'],
+  "Sharpen Filter" :   rate_units['Flops'],
+  "Blur Filter" :  rate_units['Flops'],
+  "SGEMM" : rate_units['Flops'], "DGEMM" :  rate_units['Flops'],
+  "SFFT" : rate_units['Flops'], "DFFT" :  rate_units['Flops'],
+  "N-Body" : rate_units['PairsSecond'],
+  "Ray Trace" : rate_units['PixelsSecond'],
+  "Stream Copy" : rate_units['BytesSecond'],
+  "Stream Scale" : rate_units['BytesSecond'],
+  "Stream Add" :  rate_units['BytesSecond'],
+  "Stream Triad" : rate_units['BytesSecond'] }
 
-	prefixes = ["", "K", "M", "G", "T"]
+  prefixes = ["", "K", "M", "G", "T"]
 
-	wl_unit = wl_units[wl_name]
-	divisor = 1000.0
+  wl_unit = wl_units[wl_name]
+  divisor = 1000.0
 
-	if (wl_unit == "B/sec"):
-		divisor = 1024.0
+  if (wl_unit == "B/sec"):
+    divisor = 1024.0
 
-	for prefix in range(0,4):
-		if rate < divisor:
-			break
-		rate /= divisor	
+  for prefix in range(0,4):
+    if rate < divisor:
+      break
+    rate /= divisor  
 
-	if rate > 10.0:
-		rate = round(rate, 1)
-	else:
-		rate = round(rate, 2)
+  if rate > 10.0:
+    rate = round(rate, 1)
+  else:
+    rate = round(rate, 2)
 
-	return "%s %s%s" % (rate, prefixes[prefix], wl_unit)
+  return "%s %s%s" % (rate, prefixes[prefix], wl_unit)
 
 def main():
-	jsonfile = open(sys.argv[1])
-	jsonobj = json.load(jsonfile)
-	print jsonobj['sections'][0]['workloads'][0]['results'][0]['runtime']
-	sections = get_sections(jsonobj)
-	gb_sc_score = compute_geekbench_sc_score(sections)
-	gb_mc_score = compute_geekbench_mc_score(sections)
+  jsonfile = open(sys.argv[1])
+  jsonobj = json.load(jsonfile)
+  print jsonobj['sections'][0]['workloads'][0]['results'][0]['runtime']
+  sections = get_sections(jsonobj)
+  gb_sc_score = compute_geekbench_sc_score(sections)
+  gb_mc_score = compute_geekbench_mc_score(sections)
 
 
-	for (section, wls) in sections.iteritems():
-		print section
-		for wl in wls:
-			print "\t%s".expandtabs(2) % wl.name
+  for (section, wls) in sections.iteritems():
+    print section
+    for wl in wls:
+      print "\t%s".expandtabs(2) % wl.name
 
-			wl_sc_score_str = str(int(wl.wl_sc_score())).rjust(12)
-			sc_rate_str = get_rate_string(wl.name, wl.sc_rate()).rjust(21)
-			print "\tsingle-core%s%s".expandtabs(4) % (wl_sc_score_str, sc_rate_str)
+      wl_sc_score_str = str(int(wl.wl_sc_score())).rjust(12)
+      sc_rate_str = get_rate_string(wl.name, wl.sc_rate()).rjust(21)
+      print "\tsingle-core%s%s".expandtabs(4) % (wl_sc_score_str, sc_rate_str)
 
-			wl_mc_score_str = str(int(wl.wl_mc_score())).rjust(13)
-			mc_rate_str = get_rate_string(wl.name, wl.mc_rate()).rjust(21)
-			print "\tmulti-core%s%s".expandtabs(4) % (wl_mc_score_str , mc_rate_str)
-		print ""
+      wl_mc_score_str = str(int(wl.wl_mc_score())).rjust(13)
+      mc_rate_str = get_rate_string(wl.name, wl.mc_rate()).rjust(21)
+      print "\tmulti-core%s%s".expandtabs(4) % (wl_mc_score_str , mc_rate_str)
+    print ""
 
-	print "Benchmark Summary"
-	for section in sections.iterkeys():
-		section_score_str = str(section + " Score").ljust(24)
-		sect_sc_score_str = str(compute_sect_sc_score(sections[section])).rjust(7)
-		sect_mc_score_str = str(compute_sect_mc_score(sections[section])).rjust(7)
-		print "\t%s%s%s".expandtabs(2) % (section_score_str, sect_sc_score_str, 
-		sect_mc_score_str)
-	print ""
+  print "Benchmark Summary"
+  for section in sections.iterkeys():
+    section_score_str = str(section + " Score").ljust(24)
+    sect_sc_score_str = str(compute_sect_sc_score(sections[section])).rjust(7)
+    sect_mc_score_str = str(compute_sect_mc_score(sections[section])).rjust(7)
+    print "\t%s%s%s".expandtabs(2) % (section_score_str, sect_sc_score_str, 
+    sect_mc_score_str)
+  print ""
 
-	gb_sc_score_just = str(gb_sc_score).rjust(7)
-	gb_mc_score_just = str(gb_mc_score).rjust(7)
+  gb_sc_score_just = str(gb_sc_score).rjust(7)
+  gb_mc_score_just = str(gb_mc_score).rjust(7)
 
-	print "\t%s%s%s".expandtabs(2) % ("Geekbench Score".ljust(24),
-	gb_sc_score_just, gb_mc_score_just)
-	jsonfile.close()
+  print "\t%s%s%s".expandtabs(2) % ("Geekbench Score".ljust(24),
+  gb_sc_score_just, gb_mc_score_just)
+  jsonfile.close()
 
 if __name__ == "__main__":
-	main()
+  main()
 
 
 
